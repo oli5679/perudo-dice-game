02500b92d8d4d27388b2f6dbcfe250d8da01ab9d
f4xxf4xx <felix.hubert.1@etsmtl.net>
2016-04-09 14:12:48 -0400

    Renamed package to follow PEP8 standard

--- Game/Main.py
+++ Game/Main.py
@@ -5,7 +5,7 @@ from Game import Game
 def main():
     player_number = int(raw_input("Enter the number of players: "))
     start_dice_number = int(raw_input("Enter the number of starting die per player: "))
-    game = Game(player_number, start_dice_number)
+    game = game(player_number, start_dice_number)
     game.play()
 
 if __name__ == '__main__':
